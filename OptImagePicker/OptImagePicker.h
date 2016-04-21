06013b49bd48ee4bf137bde01c65afbb62e81465
LinhLuu <linh.luu@axonactive.vn>
2016-04-21 13:12:56 +0700

    update header file

--- OptImagePicker/OptImagePicker.h
+++ OptImagePicker/OptImagePicker.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char OptImagePickerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <OptImagePicker/PublicHeader.h>
 
-
+#import <OptImagePicker/OptImagePickerController.h>
